b2b928669d84345378c69693eeab8f4c08921197
kbladewht <w00110601111@163.com>
2026-06-14 19:09:23 +0800

    add

--- pad.sh
+++ pad.sh
@@ -3,6 +3,13 @@ export PATH="/c/Users/dellht/AppData/Local/Programs/Python/Python314/Scripts:$PA
 export PATH="/c/Users/dellht/AppData/Local/Programs/Python/Python314:$PATH"
 export ZEPHYR_SDK_INSTALL_DIR="D:/zephyr-sdk-0.16.9"
 
+# Make clean
+export ORIG_CWD=$(pwd)
+. ./common.sh
+
+DRIVE_P=E
+
+
 # export MY_ZMK_DEV_DIR="D:/project/GitHub/zmk_gitee"
 
 export NRF_MODULE_DIRS="../zmk-feature-split-esb/nrf"
@@ -29,7 +36,14 @@ west build -s app -d "build/${SHIELD}" -b nice_nano_k -S zmk-usb-logging -- \
   #  -DBOARD_ROOT="/d/project/GitHub/zmkesb/zmk"
 
 
-  # cp "/d/project/GitHub/zmkesb/zmk/zmkesb/zmk/build/zephyr/zmk.uf2"
+# west -v build
+SRC_FILE="build/${SHIELD}/zephyr/zmk.uf2"
+# Generate filename with keyword, e.g., flash_left.uf2, flash_right.uf2, flash_dongle.uf2
+DEST_FILE="${DRIVE_P}:/flash_firmware.uf2"
+echo "Copying $SRC_FILE to /c/Users/dellht/Downloads/zmk_${SHIELD}.uf2"
+cp "$SRC_FILE" "/c/Users/dellht/Downloads/zmk_${SHIELD}.uf2"
 
-# zmkesb\zmk\build\donki36_left\zephyr
-cp "/d/project/GitHub/zmkesb/zmk/build/p42_right/zephyr/zmk.uf2" "E:/flash.uf2"
+  # cp "/d/project/GitHub/zmkesb/zmk/zmkesb/zmk/build/zephyr/zmk.uf2"
+polling_check
+echo "Copying $SRC_FILE to $DEST_FILE"
+cp "$SRC_FILE" "$DEST_FILE"
