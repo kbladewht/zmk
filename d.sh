481d207e3c0f4b1b84ffafc24522afffdf105987
kbladewht <w00110601111@163.com>
2026-09-17 20:34:35 +0800

    add

---
+++ d.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+export PATH="/c/Users/dellht/AppData/Local/Programs/Python/Python314/Scripts:$PATH"
+export PATH="/c/Users/dellht/AppData/Local/Programs/Python/Python314:$PATH"
+export ZEPHYR_SDK_INSTALL_DIR="D:/zephyr-sdk-0.16.9"
+
+# Make clean
+export ORIG_CWD=$(pwd)
+# . ./common.sh
+
+DRIVE_P=E
+
+
+# cd app
+rm -rf build
+rm -rf app/build
+
+cd app
+
+west build -p always -b xiao_ble//zmk -- \
+  -DSHIELD="corne_dongle prospector_adapter" \
+  -DZMK_EXTRA_MODULES="D:/project/GitHub/zmk_0917_offical/prospector-zmk-module"
